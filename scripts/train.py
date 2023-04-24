0cd4773282d5748cc56fc1404c6a3d8e9eaf8013
CUN-bjy <wnsdlqjtm@naver.com>
2023-04-24 16:41:38 +0900

    update discriminator and generator
    format by black

--- scripts/train.py
+++ scripts/train.py
@@ -9,9 +9,13 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import torch
 
+from torch.utils.tensorboard import SummaryWriter
+
 import easydict
 import numpy as np
 from tqdm import tqdm
+
+writer = SummaryWriter()
 
 
 ## visualization
@@ -21,111 +25,142 @@ def imshow(data, title="MNIST"):
     plt.savefig(f"{title}.png")
     plt.cla()
 
-def train(args, model, train_loader, test_loader, debug=False):
-    # optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
-    
+
+def train_model(args, model, train_loader, test_loader, debug=False):
     # interaction setup
     epochs = tqdm(range(args.max_iter // len(train_loader) + 1))
-    
-    # training
+
+    # training the model
     count = 0
     for epoch in epochs:
         #### TRAIN PHASE
-        model.train()
-        optimizer.zero_grad()
-        train_iterator = tqdm(enumerate(train_loader), total=len(train_loader), desc="training")
-        
+        train_iterator = tqdm(
+            enumerate(train_loader), total=len(train_loader), desc="training"
+        )
+
+        if count > args.max_iter:
+            return
+        count += 1
+
+        train_losses = []
+        recon_losses, disc_losses, gen_losses = [], [], []
         for i, batch_data in train_iterator:
-            if count > args.max_iter:
-                return model
-            count+=1
-            
             images, labels = batch_data
             images = images.view(images.size(0), -1).to(args.device)
-            
-            if debug: images = images[0].unsqueeze(0)
-            
-            mloss, _, llog = model(images)
-            
-            # backward and optimize
-            optimizer.zero_grad()
-            mloss.mean().backward()
-            optimizer.step()
-            
-            train_iterator.set_postfix({"train_loss": float(mloss.mean())})
-            if debug: break
+
+            if debug:
+                images = images[0].unsqueeze(0)
+
+            llog = model.train(images)
+
+            recon_loss, disc_loss, gen_loss = (
+                float(llog["recon_loss"].mean()),
+                float(llog["disc_loss"].mean()),
+                float(llog["gen_loss"].mean()),
+            )
+            train_loss = recon_loss + disc_loss + gen_loss
+            train_iterator.set_postfix({"train_loss": train_loss})
+            train_losses.append(train_loss)
+            recon_losses.append(recon_loss)
+            disc_losses.append(disc_loss)
+            gen_losses.append(gen_loss)
+            if debug:
+                break
+        writer.add_scalar("train/loss", sum(train_losses) / len(train_losses), epoch)
+        writer.add_scalar(
+            "train/recon_loss", sum(recon_losses) / len(recon_losses), epoch
+        )
+        writer.add_scalar("train/disc_loss", sum(disc_losses) / len(disc_losses), epoch)
+        writer.add_scalar("train/gen_loss", sum(gen_losses) / len(gen_losses), epoch)
 
         ### EVAL PHASE
-        model.eval()
-        eval_loss = 0
-        test_iterator = tqdm(enumerate(test_loader), total=len(test_loader), desc="testing")
+        test_iterator = tqdm(
+            enumerate(test_loader), total=len(test_loader), desc="testing"
+        )
+        eval_losses = []
+        recon_losses, disc_losses, gen_losses = [], [], []
         with torch.no_grad():
             for i, batch_data in test_iterator:
-                
                 images, labels = batch_data
                 images = images.view(images.size(0), -1).to(args.device)
-            
-                if debug: images = images[0].unsqueeze(0)
-                
-                mloss, recon_x, llog = model(images)
-                
-                eval_loss += mloss.mean().item()
-                
-                test_iterator.set_postfix({"eval_loss": float(mloss.mean())})
-                
+
+                if debug:
+                    images = images[0].unsqueeze(0)
+
+                llog, recon_x = model.eval(images)
+
+                recon_loss, disc_loss, gen_loss = (
+                    float(llog["recon_loss"].mean()),
+                    float(llog["disc_loss"].mean()),
+                    float(llog["gen_loss"].mean()),
+                )
+                eval_loss = recon_loss + disc_loss + gen_loss
+                test_iterator.set_postfix({"eval_loss": eval_loss})
+                eval_losses.append(eval_loss)
+                recon_losses.append(recon_loss)
+                disc_losses.append(disc_loss)
+                gen_losses.append(gen_loss)
+
                 if i == 0:
-                    nhw_orig = images.view(images.size(0), int(np.sqrt(images.size(-1))), -1)[0]
-                    nhw_recon = recon_x.view(images.size(0), int(np.sqrt(images.size(-1))), -1)[0]
+                    nhw_orig = images.view(
+                        images.size(0), int(np.sqrt(images.size(-1))), -1
+                    )[0]
+                    nhw_recon = recon_x.view(
+                        images.size(0), int(np.sqrt(images.size(-1))), -1
+                    )[0]
                     imshow(nhw_orig.cpu(), f"orig{epoch}")
                     imshow(nhw_recon.cpu(), f"recon{epoch}")
                     # writer.add_images(f"original{i}", nchw_orig, epoch)
                     # writer.add_images(f"reconstructed{i}", nchw_recon, epoch)
-                    if debug: break
-                
-        eval_loss = eval_loss / len(test_loader)
-        print(f"Evaluation Score: [{eval_loss}]")
-    
+                    if debug:
+                        break
 
-if __name__=="__main__":
+        print(f"Evaluation Score: [{sum(eval_losses)/len(eval_losses)}]")
+        writer.add_scalar("eval/loss", sum(eval_losses) / len(eval_losses), epoch)
+        writer.add_scalar(
+            "eval/recon_loss", sum(recon_losses) / len(recon_losses), epoch
+        )
+        writer.add_scalar("eval/disc_loss", sum(disc_losses) / len(disc_losses), epoch)
+        writer.add_scalar("eval/gen_loss", sum(gen_losses) / len(gen_losses), epoch)
+        if debug:
+            break
 
+
+if __name__ == "__main__":
     args = easydict.EasyDict(
         {
             "batch_size": 512,
-            "device":  torch.device("cuda")
-                    if torch.cuda.is_available()
-                    else torch.device("cpu"),
+            "device": torch.device("cuda")
+            if torch.cuda.is_available()
+            else torch.device("cpu"),
             "input_size": 784,
             "latent_size": 128,
             "learning_rate": 0.001,
             "max_iter": 1000,
-            "debug": False, 
+            "debug": False,
         }
     )
-    
-    # MNIST Dataset 
-    train_set = dsets.MNIST(root='./data', 
-                        train=True, 
-                        transform=transforms.ToTensor(),  
-                        download=True)
-    
-    test_set = dsets.MNIST(root='./data', 
-                        train=False, 
-                        transform=transforms.ToTensor(),  
-                        download=True)
+
+    # MNIST Dataset
+    train_set = dsets.MNIST(
+        root="./data", train=True, transform=transforms.ToTensor(), download=True
+    )
+
+    test_set = dsets.MNIST(
+        root="./data", train=False, transform=transforms.ToTensor(), download=True
+    )
 
     # Data Loader
-    train_loader = torch.utils.data.DataLoader(dataset=train_set, 
-                                            batch_size=args.batch_size, 
-                                            shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=test_set, 
-                                            batch_size=args.batch_size, 
-                                            shuffle=False)
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_set, batch_size=args.batch_size, shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_set, batch_size=args.batch_size, shuffle=False
+    )
 
     # Declare AAE model
     model = AAE(args.input_size, args.latent_size, device=args.device)
     model.to(args.device)
 
-    
     # Train
-    train(args, model, train_loader, test_loader, debug=args.debug)
+    train_model(args, model, train_loader, test_loader, debug=args.debug)
--- src/aae/models.py
+++ src/aae/models.py
@@ -6,73 +6,160 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from typing import Dict, Any, Tuple
+from typing import Dict, Any
 
 
 class MLP(nn.Module):
     "A simple MLP module for general purpose"
-    def __init__(self, in_dim: int, out_dim: int)-> None:
+
+    def __init__(self, in_dim: int, out_dim: int, binary_mode: bool = False) -> None:
         super().__init__()
+        self.binary_mode = binary_mode
+
         self.in_net = nn.Linear(in_dim, 512)
         self.hidden = nn.Sequential(
-            nn.Linear(512, 1024), nn.ReLU(), nn.Dropout(p=0.1), 
-            nn.Linear(1024, 512), nn.ReLU(), nn.Dropout(p=0.1),
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=0.1),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.1),
         )
         self.out_net = nn.Linear(512, out_dim)
 
-    def forward(self, X: Any)-> Any:
+    def forward(self, X: Any) -> Any:
         X = self.in_net(X)
         X = self.hidden(X)
         out = self.out_net(X)
-        return out
+        return out if not self.binary_mode else F.sigmoid(out)
 
-
-class Discriminator(nn.Module):
-    """Discriminator Model"""
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        
-    def forward(self, x):
-        return x
 
 class AAE(nn.Module):
     """Adversarial Autoencoder Model"""
-    def __init__(self, in_dim: int, latent_dim:int, device=torch.device("cuda")) -> None:
+
+    def __init__(
+        self,
+        in_dim: int,
+        latent_dim: int,
+        recon_lr: float = 1e-3,
+        reg_lr: float = 1e-5,
+        device=torch.device("cuda"),
+    ) -> None:
         super(AAE, self).__init__()
         self.device = device
-        
+
+        # models
         self._encoder = MLP(in_dim, latent_dim)
         self._decoder = MLP(latent_dim, in_dim)
-        
+        self._discriminator = MLP(latent_dim, 1, binary_mode=True)
+
+        # optimizers
+        self._recon_optim = torch.optim.Adam(
+            nn.ModuleList([self._encoder, self._decoder]).parameters(), lr=recon_lr
+        )
+        self._gen_optim = torch.optim.Adam(self._encoder.parameters(), lr=reg_lr)
+        self._disc_optim = torch.optim.Adam(
+            nn.ModuleList([self._encoder, self._discriminator]).parameters(), lr=reg_lr
+        )
+
     def encode(self, x: Any) -> Any:
         return self._encoder(x)
-    
+
     def decode(self, z: Any) -> Any:
         return self._decoder(z)
 
-    def forward(self, x: Any) -> Tuple[float, Tuple[float]]:
+    def discriminate(self, z: Any) -> Any:
+        return self._discriminator(z)
+
+    def forward(self) -> None:
+        """don't use this function"""
+        return Exception("this function is not implemented")
+
+    def train(self, x: Any) -> Dict[str, float]:
+        self._encoder.train()
+        self._decoder.train()
+        self._discriminator.train()
+
+        ###############################
+        # 1. reconstruction phase
+        ###############################
         z = self.encode(x)
         x_hat = self.decode(z)
-        
-        losses = self.loss_function(x_hat, x)
-        
-        return losses["loss"], x_hat, (losses["recon_loss"],)
 
-    def loss_function(self, *args, **kwargs) -> Dict:
-        recons = args[0]
-        input = args[1]
-        
-        recon_loss = F.mse_loss(recons, input)
-        
-        loss = recon_loss
-        
+        recon_loss = F.mse_loss(x_hat, x)
+
+        # update encoder and decoder to minimize reconstruction loss
+        self._recon_optim.zero_grad()
+        recon_loss.mean().backward()
+        self._recon_optim.step()
+
+        ###############################
+        # 2. regularation phase
+        ###############################
+        # First, update discriminator
+        self._encoder.eval()
+
+        z_fake = self.encode(x)
+        z_real = torch.randn_like(z_fake)
+
+        d_real = self.discriminate(z_real)
+        d_fake = self.discriminate(z_fake)
+
+        disc_loss = -(torch.log(d_real) + torch.log(1 - d_fake))
+
+        # backpropagate to minimize discriminative loss
+        self._disc_optim.zero_grad()
+        disc_loss.mean().backward()
+        self._disc_optim.step()
+
+        # Second, update generator
+        self._encoder.train()
+
+        z_fake = self.encode(x)
+        d_fake = self.discriminate(z_fake)
+
+        gen_loss = -torch.log(d_fake)
+
+        # backpropagate to confuse discriminaive network
+        self._gen_optim.zero_grad()
+        gen_loss.mean().backward()
+        self._gen_optim.step()
+
         return {
-            "loss": loss,
             "recon_loss": recon_loss.detach(),
+            "disc_loss": disc_loss.detach(),
+            "gen_loss": gen_loss.detach(),
         }
 
-class VAE():
+    def eval(self, x):
+        self._encoder.eval()
+        self._decoder.eval()
+        self._discriminator.eval()
+
+        # eval autoencoder
+        z_fake = self.encode(x)
+        x_hat = self.decode(z_fake)
+
+        # eval adversarial network
+        z_real = torch.randn_like(z_fake)
+
+        d_real = self.discriminate(z_real)
+        d_fake = self.discriminate(z_fake)
+
+        recon_loss = F.mse_loss(x_hat, x)
+        disc_loss = -(torch.log(d_real) + torch.log(1 - d_fake))
+        gen_loss = -torch.log(d_fake)
+
+        return {
+            "recon_loss": recon_loss.detach(),
+            "disc_loss": disc_loss.detach(),
+            "gen_loss": gen_loss.detach(),
+        }, x_hat
+
+
+class VAE:
     pass
 
-class GAN():
+
+class GAN:
     pass
